aa0d2be051e9e3297304dae89f7c9bb6682cc574
Christoph Haefner <christophhaefner@web.de>
2016-11-11 16:11:50 +0100

    Added jenkins build-script

---
+++ jenkins.sh
@@ -0,0 +1,9 @@
+DOCKERTAG=`echo $GIT_BRANCH|cut -d'/' -f 2-`
+ 
+if [ $DOCKERTAG == "master" ]; then
+	$DOCKERTAG = "latest"
+fi
+
+docker build --pull=true -t $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG .
+docker run -i -rm $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG "npm test"
+
