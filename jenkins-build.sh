20961bd0ba4c3890225dc775c27c763b97cae651
Christoph Haefner <christophhaefner@web.de>
2016-11-12 12:43:16 +0100

    Removed branch distinction from jenkins-*.sh

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -1,6 +1,2 @@
-DOCKERTAG=`echo $GIT_BRANCH|cut -d\'/\' -f 2-`
-if [ $DOCKERTAG == "master" ]; then
-	$DOCKERTAG = "latest"
-fi
-docker build --pull=true -t $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG .
+docker build --pull=true -t $DOCKER_REGISTRY/$JOB_NAME:latest .
 
--- jenkins-test.sh
+++ jenkins-test.sh
@@ -1,6 +1,2 @@
-DOCKERTAG=`echo $GIT_BRANCH|cut -d\'/\' -f 2-`
-if [ $DOCKERTAG == "master" ]; then
-	$DOCKERTAG = "latest"
-fi
-docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG "npm test"
+docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:latest "npm test"
 
