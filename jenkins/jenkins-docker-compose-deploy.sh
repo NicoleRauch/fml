ad5326875418bb1a4499c9c08658f4c86e823f70
Christoph Haefner <christophhaefner@web.de>
2016-11-12 22:42:07 +0100

    Integrated docker-compose.yml into docker-compose-deploy.sh
    To be able to generate it at build-time

--- jenkins/jenkins-docker-compose-deploy.sh
+++ jenkins/jenkins-docker-compose-deploy.sh
@@ -1,3 +1,9 @@
 echo "sudo docker-compose -p $JOB_NAME up -d";
+echo "
+fml:
+    image: $DOCKER_REGISTRY/$JOB_NAME:latest
+    ports:
+        - "3000:3000"
+" > docker-compose.yml
 sudo docker-compose -p $JOB_NAME up -d
 
