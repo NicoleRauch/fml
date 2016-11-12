6f4e473d14f04c1cb4fbde2167d0f9139f7ac71f
Christoph Haefner <christophhaefner@web.de>
2016-11-12 12:39:33 +0100

    Fixed jenkins-test.sh

--- jenkins-test.sh
+++ jenkins-test.sh
@@ -2,5 +2,5 @@ DOCKERTAG=`echo $GIT_BRANCH|cut -d\'/\' -f 2-`
 if [ $DOCKERTAG == "master" ]; then
 	$DOCKERTAG = "latest"
 fi
-docker run -i -rm $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG "npm test"
+docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:$DOCKERTAG "npm test"
 
