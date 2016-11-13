ef444baf89ede98cd405888e700b3cf04d278549
Christoph Haefner <christophhaefner@web.de>
2016-11-13 15:02:23 +0100

    Added echo debug output to jenkins-external-tests.sh

--- jenkins/jenkins-external-tests.sh
+++ jenkins/jenkins-external-tests.sh
@@ -1,14 +1,17 @@
 file=index.html
 minimumsize=2000
 
+echo "wget fml.dev.christophhaefner.de -O $file";
 wget fml.dev.christophhaefner.de -O $file;
 
 actualsize=$(wc -c <"$file")
+echo "actualisize: '$actualsize'";
+
 if [ $actualsize -ge $minimumsize ]; then
-	# everything seams fine
+	echo "file is greater than $minimumsize";
 	exit 0;
 else
-	# file looks to small
+	echo "file is smaller than $minimumsize";
 	exit 1;
 fi
 
