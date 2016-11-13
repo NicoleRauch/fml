28b63c5edc98a6b84c1bc004be02755aabfd9104
Christoph Haefner <christophhaefner@web.de>
2016-11-13 12:22:52 +0100

    Switched deploy port back to standard 3000

--- jenkins/jenkins-docker-compose-deploy.sh
+++ jenkins/jenkins-docker-compose-deploy.sh
@@ -3,7 +3,7 @@ echo "
 fml:
     image: $DOCKER_REGISTRY/$JOB_NAME:latest
     ports:
-        - "3001:3000"
+        - "3000:3000"
 " > docker-compose.yml
 sudo docker-compose -p $JOB_NAME stop
 sudo docker-compose -p $JOB_NAME rm
