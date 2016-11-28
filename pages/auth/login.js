15de1deef3333bbc584b4343c691238195c0966a
Christoph Haefner <christophhaefner@web.de>
2016-11-28 13:56:38 +0100

    Updated pages/auth/login.js to reflect current issues

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -7,7 +7,7 @@ import defaultPage from '../../hocs/defaultPage';
 const LogIn = () => (
 	<div>
 		<p>
-			Needs <Link href='https://github.com/zeit/next.js/issues/25'>#25</Link> to be fixed/implemented.
+			Needs <s><Link href='https://github.com/zeit/next.js/issues/25'>#25</Link></s>, <Link href='https://github.com/zeit/next.js/issues/291'>#291</Link> to be fixed/implemented.
 			Otherwise Next.js will not be able to work with, e.g. <Link href='https://portier.github.io/'>portier</Link>.
 			<br />
 			According to <Link href='https://github.com/zeit/next.js/issues/226#issuecomment-261390403'>this comment</Link>, shouldn't be to long from now.
