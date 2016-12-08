ce7ecdd9d1398c167e54fbcba6059b1dce360e4c
Christoph Haefner <christophhaefner@web.de>
2016-12-08 15:58:05 +0100

    Added charset utf-8 to header

--- components/header.js
+++ components/header.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export default () => (
 	<header>
 		<Head>
+			<meta charset="utf-8" />
 			<meta name='viewport' content='width=device-width, initial-scale=1' />
 			<title>FavoriteMovieList</title>
 			<meta name="theme-color" content="#303F9F" />
