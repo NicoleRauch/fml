69c363fd76f4063b0d67bc8ce5c4aad34cf78828
Christoph Haefner <christophhaefner@web.de>
2016-12-11 00:28:42 +0100

    Switch docker-run from -i to -d in jenkins/test-internal.sh

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -1,6 +1,9 @@
-echo "sudo docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test";
-ID=`sudo docker run -i $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test`
+echo "sudo docker run -i $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test";
+ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER "npm test"`
+echo "sudo docker cp $ID:coverage/ $WORKSPACE";
 sudo docker cp $ID:coverage/ $WORKSPACE
+echo "sudo docker stop $ID";
 sudo docker stop $ID
+echo "sudo docker rm --force $ID";
 sudo docker rm --force $ID
 
