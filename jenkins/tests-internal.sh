64cea987cce5449ae8a75d07aea88c2acc4df2a8
Christoph Haefner <christophhaefner@web.de>
2016-12-11 00:43:37 +0100

    Switched back from -d to -i in jenkins/tests-internal.sh

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -1,6 +1,6 @@
 set -e;
-echo "sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER bash -c 'npm test'";
-ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER bash -c 'npm test'`
+echo "sudo docker run -i $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER bash -c 'npm test'";
+ID=`sudo docker run -i $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER bash -c 'npm test'`
 echo "sudo docker cp $ID:coverage/ $WORKSPACE";
 sudo docker cp $ID:coverage/ $WORKSPACE
 echo "sudo docker stop $ID";
