3ba54b9063936d5594b6bd7021358b2db1601d7c
Christoph Haefner <christophhaefner@web.de>
2016-12-11 00:19:36 +0100

    Copy coverage report to workspace in jenkins/test-internal.sh

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -1,3 +1,6 @@
 echo "sudo docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test";
-sudo docker run -i --rm $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test
+ID=`sudo docker run -i $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER npm test`
+sudo docker cp $ID:coverage/ $WORKSPACE
+sudo docker stop $ID
+sudo docker rm --force $ID
 
