0e22123748c530f210ec65d4ae031fa36c9f4b78
Christoph Haefner <christophhaefner@web.de>
2016-12-11 00:34:48 +0100

    Updated echos and added --no-color to jenkins/deploy-*.sh

--- jenkins/deploy-beta.sh
+++ jenkins/deploy-beta.sh
@@ -1,12 +1,14 @@
-echo "sudo docker-compose -p $JOB_NAME-beta -f docker-compose-beta.yml up -d --force-recreate";
 echo "
 webapp:
     image: $DOCKER_REGISTRY/$JOB_NAME:latest
     ports:
         - "3001:3000"
 " > docker-compose-beta.yml
-sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml stop
+echo "sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml stop";
+sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml stop
 # -v => Remove volumes associated with containers
-sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml rm --force -v
-sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate
+echo "sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml rm --force -v";
+sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml rm --force -v
+echo "sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate";
+sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate
 
