48d8f258c831912a8774952453975ace3f157b9d
Christoph Haefner <christophhaefner@web.de>
2016-12-11 10:14:12 +0100

    Removed --no-color from docker-compose up in jenkins/deploy-*.sh

--- jenkins/deploy-beta.sh
+++ jenkins/deploy-beta.sh
@@ -10,6 +10,6 @@ sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml stop
 # -v => Remove volumes associated with containers
 echo "sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml rm --force -v";
 sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml rm --force -v
-echo "sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate";
-sudo docker-compose --no-color -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate
+echo "sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate";
+sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml up -d --force-recreate
 
