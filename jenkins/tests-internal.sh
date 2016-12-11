cb07b9ac9b44f0f7d5432d54a1c897ab6c321cf7
Christoph Haefner <christophhaefner@web.de>
2016-12-11 09:34:57 +0100

    Use absolute path for docker-cp

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -3,8 +3,8 @@ echo "sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER";
 ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER`
 echo "sudo docker exec -i $ID npm test;";
 sudo docker exec -i $ID npm test;
-echo "sudo docker cp $ID:coverage/ $WORKSPACE";
-sudo docker cp $ID:coverage/ $WORKSPACE
+echo "sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/";
+sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/
 echo "sudo docker stop $ID";
 sudo docker stop $ID
 echo "sudo docker rm --force $ID";
