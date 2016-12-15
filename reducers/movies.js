b89de254adc0e4ecbcf7841f012f75acc61e566a
Christoph Haefner <christophhaefner@web.de>
2016-12-15 15:48:56 +0100

    Added ADD_MOVIES_LINE-BY-LINE to movies-reducer

--- reducers/movies.js
+++ reducers/movies.js
@@ -1,4 +1,37 @@
-export default (state = [], action) => {
-	return state;
+const movie = (state, action) => {
+	switch(action.type) {
+		case 'ADD_MOVIE':
+			return {
+				id: hashCode(action.title),
+				title: action.title
+			};
+		default:
+			return state;
+	}
 };
+
+export default (state = [], action) => {
+	switch(action.type) {
+		case 'ADD_MOVIES_LINE-BY-LINE':
+			return state.concat(action.value.movies
+				.split("\n")
+				.map((movieName) => movie(undefined, {
+					type: 'ADD_MOVIE',
+					title: movieName
+				})));
+		default:
+			return state;
+	}
+};
+
+const hashCode = (titleString) => {
+	let hash = 0, length = titleString.length;
+	if (length == 0) return hash;
+	for (let i = 0; i < length; i++) {
+		let character = titleString.charCodeAt(i);
+		hash = ((hash<<5)-hash)+character;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+}
 
--- tests/reducer.test.js
+++ tests/reducer.test.js
@@ -6,5 +6,36 @@ describe('movies reducer', () => {
 			reducer(undefined, {})
 		).toEqual({"movies": []})
 	})
+
+	it('should add movies line-by-line', () => {
+		const stateBefore = {movies: [{
+			id: -662285847,
+			title: 'Mission Impossible'
+		}]};
+		const action = {
+			type: 'ADD_MOVIES_LINE-BY-LINE',
+			value: {
+				movies: 'Matrix\nStar Wars\nHerr der Ringe'
+			}
+		};
+
+		expect(
+			reducer(stateBefore, action)
+		).toEqual({
+			movies:[{
+			id: -662285847,
+			title: 'Mission Impossible'
+			}, {
+				id: -1997372447,
+				title: "Matrix"
+			}, {
+				id: -202820711,
+				title: "Star Wars"
+			}, {
+				id: -1801708797,
+				title: "Herr der Ringe"
+			}]
+		});
+	})
 })
 
