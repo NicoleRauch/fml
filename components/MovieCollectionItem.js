df18e8a444321960b77dbd1f7bff8ab6290928c9
Christoph Haefner <christophhaefner@web.de>
2016-12-18 00:31:28 +0100

    Added Show-Link to MovieCollectionItem

--- components/MovieCollectionItem.js
+++ components/MovieCollectionItem.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link';
 
-export default ({ name }) => (
+export default ({ id, name }) => (
 	/*
 	 * Hugo (author) [100%]
 	 * Datengrab (author) [50% => 20 left]
 	 */
 	<li>
-		{name}
+		{name} <Link href={'/movieCollection?id='+id}>Show</Link>
 	</li>
 )
 
