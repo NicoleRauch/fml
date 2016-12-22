693afab186d94fdbf4e0ae230283ebd4ee966d54
Christoph Haefner <christophhaefner@web.de>
2016-12-22 16:57:35 +0100

    Updated containers/AddListInputTextfield
    Removed unused ADD_MOVIES_LINE-BY-LINE dispatch
    Redirect to movieCollections instead of rate

--- containers/AddListInputTextfield.js
+++ containers/AddListInputTextfield.js
@@ -8,13 +8,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 				type: 'ADD_MOVIECOLLECTION_WITH_MOVIES_LINE-BY-LINE',
 				movieCollection
 			});
-			dispatch({
-				type: 'ADD_MOVIES_LINE-BY-LINE',
-				value: {
-					movies: movieCollection.movies
-				}
-			});
-			ownProps.url.pushTo('/rate');
+			ownProps.url.pushTo('/movieCollections');
 		}
 	}
 }
