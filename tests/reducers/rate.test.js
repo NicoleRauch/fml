e7a1e98d869097e8ac0a3123182d57d76e60d642
Christoph Haefner <christophhaefner@web.de>
2016-12-23 17:28:53 +0100

    Added dummy rate reducer test

---
+++ tests/reducers/rate.test.js
@@ -0,0 +1,15 @@
+import reducerTest from '../helper/reducerTest'
+import reducer from '../../reducers/rate'
+const it = reducerTest.bind(null, reducer);
+
+describe('rate reducer', () => {
+	it('should put the first film into the rated list automatically', {
+		stateBefore: {
+		},
+		action: {
+		},
+		stateAfter: {
+		}
+	});
+});
+
