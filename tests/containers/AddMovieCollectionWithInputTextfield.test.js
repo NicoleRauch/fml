8a33761ae6b27e7c3f00f819eada96338c4c331c
Christoph Haefner <christophhaefner@web.de>
2016-12-24 22:21:20 +0100

    Test that AddMovieCollectionWithInputTextfield redirects after submit

--- tests/containers/AddMovieCollectionWithInputTextfield.test.js
+++ tests/containers/AddMovieCollectionWithInputTextfield.test.js
@@ -18,5 +18,32 @@ describe("FilteredMovieList", () => {
 
 		expect(wrapper.children().length).toBeGreaterThan(0);
 	});
+
+	it('should redirect after submit', () => {
+		// redirect means call url.pushTo with a non-zero string
+		const mockFn = jest.fn();
+		const props = {
+			url: {
+				pushTo: mockFn
+			}
+		};
+		const wrapper = mount(
+			<Provider store={store}>
+				<Component url={props.url}/>
+			</Provider>
+		);
+
+		wrapper.find('form').simulate('submit', {
+			name: 'Awesome Vol1',
+			movies: 'One movie per line...'
+		});
+		expect(mockFn).toBeCalled();
+		// first argument should be a non-zero length string
+		// and contain a'/'
+		const firstParam = mockFn.mock.calls[0][0];
+		expect(firstParam).toBeTruthy();
+		expect(firstParam.length).toBeGreaterThan(0);
+		expect(firstParam).toContain('/');
+	});
 });
 
