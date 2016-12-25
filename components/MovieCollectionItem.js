ad81cdf1f066ef1ba083768bd3391d246a771a6d
Christoph Haefner <christophhaefner@web.de>
2016-12-25 12:52:39 +0100

    Added comment about spinner gif to MovieCollectionItem

--- components/MovieCollectionItem.js
+++ components/MovieCollectionItem.js
@@ -5,6 +5,8 @@ export default ({ id, name }) => (
 	/*
 	 * Hugo (author) [100%]
 	 * Datengrab (author) [50% => 20 left]
+	 * if it's still loading from file, display spinner like
+	 *  (http://spiffygif.com/gif?color=000000&length=2&radius=3&width=1 -> 14x14px)
 	 */
 	<li>
 		{name} <Link href={'/movieCollection?id='+id}>Show</Link> <Link href={'/rate?id='+id}>Rate</Link>
