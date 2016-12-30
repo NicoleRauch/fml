0ce70a8f132c308aed3b8e29ebb74430a2023563
Christoph Haefner <christophhaefner@web.de>
2016-12-30 21:02:37 +0100

    Use 'import *' syntax for action imports

--- containers/AddMovieCollectionWithInputTextfield.js
+++ containers/AddMovieCollectionWithInputTextfield.js
@@ -1,11 +1,11 @@
-import { connect } from 'react-redux'
-import InputTextfield from '../components/CreateMovieCollectionWithTextareaForm'
-import { addMovieCollectionWithMoviesLineByLine } from '../actions/'
+import { connect } from 'react-redux';
+import InputTextfield from '../components/CreateMovieCollectionWithTextareaForm';
+import * as Actions from '../actions/';
 
 const mapDispatchToProps = (dispatch, ownProps) => {
 	return {
 		onSubmit: (movieCollection) => {
-			dispatch(addMovieCollectionWithMoviesLineByLine(movieCollection));
+			dispatch(Actions.addMovieCollectionWithMoviesLineByLine(movieCollection));
 			ownProps.url.pushTo('/movieCollections');
 		}
 	}
