849086f784828e84dff9d8071a955c51c320f75e
Christoph Haefner <christophhaefner@web.de>
2017-01-02 23:54:15 +0100

    Enhanced MovieCollectionItem with isLoading

--- components/MovieCollectionItem.js
+++ components/MovieCollectionItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 
-export default ({ id, name }) => (
+export default ({ id, name, isLoading }) => (
 	/*
 	 * Hugo (author) [100%]
 	 * Datengrab (author) [50% => 20 left]
@@ -9,7 +9,16 @@ export default ({ id, name }) => (
 	 *  (http://spiffygif.com/gif?color=000000&length=2&radius=3&width=1 -> 14x14px)
 	 */
 	<li>
-		{name} <Link href={'/movieCollection?id='+id}>Show</Link> <Link href={'/rate?id='+id}>Rate</Link>
+		{name} { isLoading && <LoadingMovieCollection />} { !isLoading && <AvailableMovieCollection id={id} /> }
 	</li>
 )
 
+const AvailableMovieCollection = ({ id }) => (
+	<span>
+		<Link href={'/movieCollection?id='+id}>Show</Link> <Link href={'/rate?id='+id}>Rate</Link>
+	</span>
+)
+
+const LoadingMovieCollection = () => (
+	<img src="/static/loadingSpinner14x14.gif" alt="Loading Spinner" /> 
+)
