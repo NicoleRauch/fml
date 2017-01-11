db9c7f929037d8b88e734f6996a8eb629886cf8d
Christoph Haefner <christophhaefner@web.de>
2017-01-11 22:55:48 +0100

    Removed unused method in container component

--- containers/MovieCollectionSpecificMovieList.js
+++ containers/MovieCollectionSpecificMovieList.js
@@ -1,11 +1,6 @@
 import { connect } from 'react-redux'
 import MovieList from '../components/MovieList'
 
-const getMovieCollectionMoviesById = (state, collectionId) => {
-	const movieCollection = state.movieCollections[collectionId]
-	return movieCollection ? movieCollection.movies : [];
-};
-
 const mapStateToProps = (state, ownProps) => ({
 	movies: getSortedMovies(state, ownProps.collectionId)
 });
