ea1d44b681061986a90d04b900150023155958b5
Christoph Haefner <christophhaefner@web.de>
2017-01-19 21:54:21 +0100

    Implemented lastMovie case in rateResultsToMovieArray

--- tests/lib/rateResultsToMovieArray.test.js
+++ tests/lib/rateResultsToMovieArray.test.js
@@ -16,4 +16,32 @@ describe("RateResultsToMovieArray", () => {
 		}]);
 		expect(actual).toEqual(expected);
 	});
+
+	it('should sort second movie behind first if it lost the comparison', () => {
+		const expected = ['1931101123', '1997372447'];
+		const actual = SuT([{
+			movieFromCollection: '1931101123', // Pulp Fiction
+			movieFromPersonalList: undefined,
+			won: true
+		}, {
+			movieFromCollection: '1997372447', // Matrix
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: false
+		}]);
+		expect(actual).toEqual(expected);
+	});
+
+	it('should sort second movie before first if it won the comparison', () => {
+		const expected = ['1997372447', '1931101123'];
+		const actual = SuT([{
+			movieFromCollection: '1931101123', // Pulp Fiction
+			movieFromPersonalList: undefined,
+			won: true
+		}, {
+			movieFromCollection: '1997372447', // Matrix
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: true
+		}]);
+		expect(actual).toEqual(expected);
+	});
 });
