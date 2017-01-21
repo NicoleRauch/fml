ad110b7e8e9d3fee514201424158bc6312018052
Christoph Haefner <christophhaefner@web.de>
2017-01-21 11:51:30 +0100

    Implemented next movie case, tested with three movies

--- tests/lib/rateResultsToMovieArray.test.js
+++ tests/lib/rateResultsToMovieArray.test.js
@@ -17,7 +17,7 @@ describe("RateResultsToMovieArray", () => {
 		expect(actual).toEqual(expected);
 	});
 
-	it('should sort second movie behind first if it lost the comparison', () => {
+	it('should sort two movies second lost', () => {
 		const expected = ['1931101123', '1997372447'];
 		const actual = SuT([{
 			movieFromCollection: '1931101123', // Pulp Fiction
@@ -30,8 +30,7 @@ describe("RateResultsToMovieArray", () => {
 		}]);
 		expect(actual).toEqual(expected);
 	});
-
-	it('should sort second movie before first if it won the comparison', () => {
+	it('should sort two movies second won', () => {
 		const expected = ['1997372447', '1931101123'];
 		const actual = SuT([{
 			movieFromCollection: '1931101123', // Pulp Fiction
@@ -44,4 +43,65 @@ describe("RateResultsToMovieArray", () => {
 		}]);
 		expect(actual).toEqual(expected);
 	});
+
+	it('should sort three movies second won, third lost/lost', () => {
+		const expected = ['1997372447', '1931101123', '906876631'];
+		const actual = SuT([{
+			movieFromCollection: '1931101123', // Pulp Fiction
+			movieFromPersonalList: undefined,
+			won: true
+		}, {
+			movieFromCollection: '1997372447', // Matrix
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: true
+		}, {
+			movieFromCollection: '906876631', // Stirb Langsam
+			movieFromPersonalList: '1997372447', // Matrix
+			won: false
+		}, {
+			movieFromCollection: '906876631', // Stirb Langsam
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: false
+		}]);
+		expect(actual).toEqual(expected);
+	});
+	it('should sort three movies second won, third lost/won', () => {
+		const expected = ['1997372447', '906876631', '1931101123'];
+		const actual = SuT([{
+			movieFromCollection: '1931101123', // Pulp Fiction
+			movieFromPersonalList: undefined,
+			won: true
+		}, {
+			movieFromCollection: '1997372447', // Matrix
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: true
+		}, {
+			movieFromCollection: '906876631', // Stirb Langsam
+			movieFromPersonalList: '1997372447', // Matrix
+			won: false
+		}, {
+			movieFromCollection: '906876631', // Stirb Langsam
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: true
+		}]);
+		expect(actual).toEqual(expected);
+	});
+	it('should sort three movies second won, third won', () => {
+		const expected = ['906876631', '1997372447', '1931101123'];
+		const actual = SuT([{
+			movieFromCollection: '1931101123', // Pulp Fiction
+			movieFromPersonalList: undefined,
+			won: true
+		}, {
+			movieFromCollection: '1997372447', // Matrix
+			movieFromPersonalList: '1931101123', // Pulp Fiction
+			won: true
+		}, {
+			movieFromCollection: '906876631', // Stirb Langsam
+			movieFromPersonalList: '1997372447', // Matrix
+			won: true
+		}]);
+		expect(actual).toEqual(expected);
+	});
 });
+
