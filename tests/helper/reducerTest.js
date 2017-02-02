a042ff9c8019ddaad334b0b6fc8971d33a822d3f
Christoph Haefner <christophhaefner@web.de>
2017-02-02 11:02:50 +0100

    Enhance helper/reducerTest to handle multiple actions

--- tests/helper/reducerTest.js
+++ tests/helper/reducerTest.js
@@ -2,7 +2,15 @@ import deepFreeze from 'deep-freeze'
 
 export default (reducer, description, params) => {
 	it(description, () => {
-		const { stateBefore, action, stateAfter } = params;
+		const { stateBefore, action, actions, stateAfter } = params;
+
+		if(Array.isArray(actions)) {
+			const actual = actions.reduce((state, action) => {
+				deepFreeze(action);
+				return reducer(state, action);
+			}, stateBefore);
+			expect(actual).toEqual(stateAfter);
+		}
 
 		if(stateBefore) deepFreeze(stateBefore);
 		deepFreeze(action);
