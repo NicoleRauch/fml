4bca1dce6ddfa31077d9d1afa03f4598884ee3ed
Christoph Haefner <christophhaefner@web.de>
2017-02-11 16:33:42 +0100

    tests-internal should clean-up even if test fails

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -1,12 +1,12 @@
+set -x;
 set -e;
-echo "sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER";
-ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER`
-echo "sudo docker exec -i $ID npm test;";
-sudo docker exec -i $ID npm test;
-echo "sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/";
+
+# Define cleanup, no matter how the script exits
+trap '
 sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/
-echo "sudo docker stop $ID";
 sudo docker stop $ID
-echo "sudo docker rm --force $ID";
-sudo docker rm --force $ID
+sudo docker rm --force $ID' EXIT
+
+ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER`
+sudo docker exec -i $ID npm test;
 
