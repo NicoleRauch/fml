602d074415482c383a57f73268ca33ba24d19d97
Christoph Haefner <christophhaefner@web.de>
2017-02-17 06:22:07 +0100

    Move coverage copy from trap into normal script

--- jenkins/tests-internal.sh
+++ jenkins/tests-internal.sh
@@ -3,10 +3,10 @@ set -e;
 
 # Define cleanup, no matter how the script exits
 trap '
-sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/
 sudo docker stop $ID
 sudo docker rm --force $ID' EXIT
 
 ID=`sudo docker run -d $DOCKER_REGISTRY/$JOB_NAME:$BUILD_NUMBER`
 sudo docker exec -i $ID npm test;
+sudo docker cp $ID:/usr/src/app/coverage/ $WORKSPACE/
 
