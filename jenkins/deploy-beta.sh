66e9f7d3ce1ee8b9c353d7bdeed307149f200b4e
Christoph Haefner <christophhaefner@web.de>
2017-03-13 18:12:41 +0100

    Add NODE_ENV to jenkins/deploy*.sh scripts

--- jenkins/deploy-beta.sh
+++ jenkins/deploy-beta.sh
@@ -5,6 +5,8 @@ webapp:
     image: $DOCKER_REGISTRY/$JOB_NAME:latest
     ports:
         - "3001:3000"
+	environment:
+		NODE_ENV: production
 " > docker-compose-beta.yml
 sudo docker-compose -p "$JOB_NAME-beta" -f docker-compose-beta.yml stop
 # -v => Remove volumes associated with containers
--- jenkins/deploy-dev.sh
+++ jenkins/deploy-dev.sh
@@ -5,6 +5,8 @@ webapp:
     image: $DOCKER_REGISTRY/$JOB_NAME:latest
     ports:
         - "3000:3000"
+	enviornment:
+		NODE_ENV: dev
 " > docker-compose-dev.yml
 sudo docker-compose -p "$JOB_NAME-dev" -f docker-compose-dev.yml stop
 # -v => Remove volumes associated with containers
