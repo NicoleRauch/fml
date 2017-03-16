e7ce5c230c64a7980b0bbbd5180eac0761c21950
Christoph Haefner <christophhaefner@web.de>
2017-03-16 09:56:52 +0100

    Fix typo enviornment -> environment in jenkins/deploy-dev.sh

--- jenkins/deploy-dev.sh
+++ jenkins/deploy-dev.sh
@@ -5,7 +5,7 @@ webapp:
     image: $DOCKER_REGISTRY/$JOB_NAME:latest
     ports:
         - "3000:3000"
-    enviornment:
+    environment:
         NODE_ENV: dev
 " > docker-compose-dev.yml
 sudo docker-compose -p "$JOB_NAME-dev" -f docker-compose-dev.yml stop
